fda64b7fda3487effd25f37d90681912bdb98972
Lucely Torres <yubelytorres2214@gmail.com>
2025-08-11 11:12:42 -0500

    //Realizamos un if para implementar una validación para asegurarse de que el campo no esté vacío.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 let amigos = []; // Creo la variable "amigos" que es donde se almacenara el nombre de las personas que van a ser parte del sorteo function agregarAmigo
+//se crea la funcion que va a almacenar los nombres ingresados.
+function agregarAmigo() {
+const input = document.getElementById ("amigo");
+const nombre =  input.value.trim(); // trim: quita espacios al inicio y final.
+
+//Realizamos un if para implementar una validación para asegurarse de que el campo no esté vacío.
+if (nombre === ""){
+    alert("Por favor, inserte un nombre.");
+    input.focus(); // Devuelve el cursor al campo para que pueda escribir.
+    return;
+}
+}
