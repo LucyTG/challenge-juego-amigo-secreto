0fd9934749cd587b632517b081e38585e4526e6b
Lucely Torres <yubelytorres2214@gmail.com>
2025-08-11 11:30:38 -0500

    Se crea la funcion que nos va ayudar a actualizar la lista y se obtiene el elemento de la lista.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let amigos = []; // Creo la variable "amigos" que es donde se almacenara el nombre de las personas que van a ser parte del sorteo function agregarAmigo
-//se crea la funcion que va a almacenar los nombres ingresados.
+//Se crea la funcion que va a almacenar los nombres ingresados.
 function agregarAmigo() {
 const input = document.getElementById ("amigo");
 const nombre =  input.value.trim(); // trim: quita espacios al inicio y final.
@@ -12,5 +12,11 @@ if (nombre === ""){
 }
 amigos.push(nombre); // Para agregar un nuevo amigo a la lista.
 input.value = ""; //Borra el texto que estaba en el campo para que el usuario pueda escribir otro nombre.
+actualizarLista(); //Actualiza la lista en la pantalla.
+}
+//Se crea la funcion que nos va ayudar a actualizar la lista.
+function actualizarLista(){
+const lista = document.getElementById("listaAmigos");
 
 }
+
